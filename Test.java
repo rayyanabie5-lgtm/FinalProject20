170828d73aa21e3649a1ab904d399ac87096b62d
254107020205 <254107020205@student.polinema.ac.id>
2025-09-24 15:34:03 +0700

    commit ega

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hi, ini baris kode pertama");
-        
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user");
+        System.out.println("User 1 menambahkan baris baru");
 
         
     }
